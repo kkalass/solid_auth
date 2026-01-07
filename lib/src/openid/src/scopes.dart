8d5b17345e1ea83f1418d628b98080dd0b7ca66a
Graham Williams <graham.williams@togaware.com>
2026-01-07 14:31:11 +1100

    Reduce size of openid.dart - extract scopesSupported.

---
+++ lib/src/openid/src/scopes.dart
@@ -0,0 +1,72 @@
+// Copyright (c) 2017, Rik Bellens.
+// All rights reserved.
+
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of the <organization> nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+// ignore_for_file: depend_on_referenced_packages
+
+const supportedScopes = [
+  'public_profile',
+  'user_friends',
+  'email',
+  'user_about_me',
+  'user_actions.books',
+  'user_actions.fitness',
+  'user_actions.music',
+  'user_actions.news',
+  'user_actions.video',
+  'user_birthday',
+  'user_education_history',
+  'user_events',
+  'user_games_activity',
+  'user_hometown',
+  'user_likes',
+  'user_location',
+  'user_managed_groups',
+  'user_photos',
+  'user_posts',
+  'user_relationships',
+  'user_relationship_details',
+  'user_religion_politics',
+  'user_tagged_places',
+  'user_videos',
+  'user_website',
+  'user_work_history',
+  'read_custom_friendlists',
+  'read_insights',
+  'read_audience_network_insights',
+  'read_page_mailboxes',
+  'manage_pages',
+  'publish_pages',
+  'publish_actions',
+  'rsvp_event',
+  'pages_show_list',
+  'pages_manage_cta',
+  'pages_manage_instant_articles',
+  'ads_read',
+  'ads_management',
+  'business_management',
+  'pages_messaging',
+  'pages_messaging_subscriptions',
+  'pages_messaging_phone_number',
+];
